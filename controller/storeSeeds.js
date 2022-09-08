af375a859b0f1d2dcf385cd3d4c1f870d63a15e8
Jaden Garrity <jaden@Jadens-MBP.attlocal.net>
2022-09-08 04:04:06 -0400

    updated code with help from friend

---
+++ controller/storeSeeds.js
@@ -0,0 +1,111 @@
+//kyler helped me link my code for this section. 
+//DEPENDENCIES
+const express = require('express')
+const productsRouter = express.Router(); //this basically creates the route that will connect to my route section in my server.js
+const Store = require ('../models/products')// connects to my products in the models folder
+
+//SEED
+productsRouter.get('/seed', (req,res) => {
+  Store.create(
+
+    [
+        {
+          name: 'Beans',
+          description: 'A small pile of beans. Buy more beans for a big pile of beans.',
+          img: 'https://imgur.com/LEHS8h3.png',
+          price: 5,
+          qty: 99
+        }, {
+          name: 'Bones',
+          description: "It's just a bag of bones.",
+          img: 'https://imgur.com/dalOqwk.png',
+          price: 25,
+          qty: 0
+        }, {
+          name: 'Bins',
+          description: 'A stack of colorful bins for your beans and bones.',
+          img: 'https://imgur.com/ptWDPO1.png',
+          price: 7000,
+          qty: 1
+        },
+      ],
+      (error, data) => {
+        res.redirect('/products');
+      }
+  );
+});
+//CODE BELOW I TOOK FROM MY SERVER.JS
+
+// Index
+productsRouter.get('/', (req, res) => {
+	Store.find({}, (error, allProducts) => {
+		res.render('index.ejs', {
+			products: allProducts,
+		});
+	});
+});
+
+// NEW
+productsRouter.get("/new", (req, res) => {
+    res.render('new.ejs')
+});
+
+// DELETE
+productsRouter.delete('/:id', (req,res)=>{
+  Store.findByIdAndDelete(req.params.id, (error, foundItem) =>{
+    productId = req.params.id;
+    res.redirect('./products')
+  })
+})
+
+// UPDATE
+productsRouter.put('/:id', (req, res) => {
+  const newItem = {
+      name: req.body.name,
+      description: req.body.description,
+      img: req.body.img,
+      price: req.body.price,
+      qty: req.body.qty,
+  }
+  products.findByIdAndUpdate(req.params.id, newItem, (error, foundItem) => {
+      foundItem[req.params.id] = newItem;
+      res.redirect(`/products/${req.params.id}`)
+  })
+});
+
+// CREATE
+productsRouter.post("/", (req, res) => {
+  const newProduct = {
+    name: req.body.name,
+    description: req.body.description,
+    img: req.body.img,
+    price: req.body.price,
+    qty: req.body.qty
+  }
+    Store.create(newProduct, (error, foundItem) => {
+        res.redirect('/products');
+    });
+})
+
+// EDIT
+productsRouter.get('/:id/edit', (req, res) => {
+  Store.findById(req.params.id, (err, foundItem) => {
+      res.render('edit.ejs', {
+          product: foundItem,
+          productId: req.params.id
+      })
+  })
+});
+
+// SHOW
+productsRouter.get('/:id', (req, res) => {
+	Store.findById(req.params.id, (err, foundItem) => {
+		res.render("show.ejs", {
+			product: foundItem,
+      productId: req.params.id
+		});
+	});
+});
+
+//exports
+module.exports = productsRouter;
--- server.js
+++ server.js
@@ -3,14 +3,19 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const mongoose = require("mongoose");
-const Store = require("./models/products.js")
-const seed = 
+const methodOverride = require("method-override")
+
+//Middleware
+app.use(express.urlencoded({extended:false}));
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
 
 // DATABASE CONFIGURATION
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
 
 // Database Connection Error/Success
 // Define callback functions for various events
@@ -19,57 +24,10 @@ db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
 db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
-// MIDDLEWARE  & BODY PARSER
-app.use(express.urlencoded({ extended: true }));
-
 // ROUTES
-// app.get('/products/seed', (req, res) => {
-//     Product.deleteMany({}, (error, allProducts) => {})
-//     Product.create(storeSeed, (error, data) => {
-//         res.redirect("/products")
-//     })
-// })
+const productsController = require('./controller/storeSeeds');
+app.use('/products', productsController)
 
-// Index
-app.get('/products', (req, res) => {
-	Store.find({}, (error, allProducts) => {
-		res.render('index.ejs', {
-			products: allProducts,
-		});
-	});
-});
-
-
-// NEW
-app.get("/products/new", (req, res) => {
-    res.render('new.ejs')
-});
-
-// DELETE
-
-
-// UPDATE
-
-
-// CREATE
-app.post("/products", (req, res) => {
-    Store.create(req.body, (error, createdStore) => {
-        res.send(createdBook);
-    });
-})
-
-// EDIT
-
-
-// SHOW
-app.get('/products/:id', (req, res) => {
-	Store.findById(req.params.id, (err, foundStore) => {
-		res.render("show.ejs", {
-			product: foundStore
-		});
-
-	});
-});
 
 // LISTENER
 const PORT = process.env.PORT;
